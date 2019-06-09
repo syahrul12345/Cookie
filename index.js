3dc3753f3f2ceaaff5b5aada1dbc60a12a6fb76d
Muhd Syahrul Nizam <muhdsyahrulnizam@Muhds-MacBook-Pro.local>
2019-06-09 18:30:21 +0800

    Ability to connect to node, but cannot deploy yet

--- index.js
+++ index.js
@@ -11,14 +11,15 @@ const fs = require('fs')
 program
   .option('new <type>','Creates new project directory')
   .option('compile','Compiles solidity smart contracts (only)')
-  .option('deploy','Deploy the smart contract(only)')
+  .option('deploy [config]','Deploy the smart contract(only)')
+
 //parse arguments
 program.parse(process.argv);
 //handle the different flags
 if (program.new){
   //creates a new project in current folder
   //__dirname is the current folder, use this
-  folder.create(process.cwd() + slash + `${program.new}`,slash,function(result) {
+  folder.create(path.resolve(process.cwd(),`${program.new}`),function(result) {
     if (result) console.log(result)
   })
 }
@@ -31,12 +32,21 @@ if (program.compile) {
     })
   })
 }
-if (program.deploy) {
+if (program.deploy === true) {
   //get the bin and deploy
   fs.readdir(path.resolve(process.cwd(),'build'),function(err,files) {
     files.forEach(function(file){
       if(path.extname(file) == '.bin'){
-        Deploy.deploy(path.resolve(process.cwd(),'build',file))
+        Deploy.deploy(path.resolve(process.cwd(),'build',file),'default')
+      }
+    })
+  })
+}
+else if(program.deploy){
+  fs.readdir(path.resolve(process.cwd(),'build'),function(err,files) {
+    files.forEach(function(file){
+      if(path.extname(file) == '.bin'){
+        Deploy.deploy(path.resolve(process.cwd(),'build',file),`${program.deploy}`)
       }
     })
   })
