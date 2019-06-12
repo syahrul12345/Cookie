769fb748cafc4092ec0b2e239388cbb03d46dc85
Muhd Syahrul Nizam <mudhsyahrulnizam123@gmail.com>
2019-06-12 11:36:15 +0800

    Deployment tryouts

--- index.js
+++ index.js
@@ -2,17 +2,19 @@
 // dependencies
 const program = require('commander')
 const folder = require('./lib/folder.js')
-const Compile = require('./lib/compile.js')
-const Deploy = require('./lib/deploy.js')
 const path = require('path')
 const process = require('process')
 const fs = require('fs')
-
+//custom lbiraries
+const Compile = require('./lib/compile.js')
+const Deploy = require('./lib/deploy.js')
+const Generate = require('./lib/generate.js')
 program
   .option('new <type>','Creates new project directory')
   .option('compile','Compiles solidity smart contracts (only)')
+  .option('generate','Generates the schematic for Angular frontend only')
   .option('deploy [config]','Deploy the smart contract(only)')
-
+  .option('bake [config]','Compile & Deploy smart contract. Generate a front-end application')
 //parse arguments
 program.parse(process.argv);
 //handle the different flags
@@ -44,6 +46,43 @@ else if(program.deploy){
   fs.readdir(path.resolve(process.cwd(),'build'),function(err,files) {
     files.forEach(function(file){
       Deploy.deploy(path.resolve(process.cwd(),'build',file),`${program.deploy}`)
+    })
+  })
+}
+if(program.bake === true) {
+  fs.readdir(path.resolve(process.cwd(),'contract'),function(err,files) {
+    files.forEach(function(file) {
+      fullPath = path.resolve(process.cwd(),'contract',file)
+      Compile.compile(fullPath)
+    })
+  })
+  fs.readdir(path.resolve(process.cwd(),'build'),function(err,files) {
+    files.forEach(function(file){
+      Deploy.deploy(path.resolve(process.cwd(),'build',file),'default')
+    })
+  })
+}else if(program.bake){
+  fs.readdir(path.resolve(process.cwd(),'contract'),function(err,files) {
+    files.forEach(function(file) {
+      fullPath = path.resolve(process.cwd(),'contract',file)
+      Compile.compile(fullPath)
+    })
+  })
+  fs.readdir(path.resolve(process.cwd(),'build'),function(err,files) {
+    files.forEach(function(file){
+      Deploy.deploy(path.resolve(process.cwd(),'build',file),`${program.deploy}`)
+    })
+  })
+}
+if(program.generate){
+  fs.readdir(path.resolve(process.cwd(),'contract'),function(err,files) {
+    files.forEach(function(file) {
+      Generate.generate(path.resolve(process.cwd(),'contract',file))
+    })
+  })
+  fs.readdir(path.resolve(process.cwd(),'schematic'),function(err,files){
+    files.forEach(function(file){
+      console.log(JSON.parse(fs.readFileSync(path.resolve(process.cwd(),'schematic',file))))
     })
   })
 }
